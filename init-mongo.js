c4b805541edf18db0bc1aff628eef8c7accbe113
michael byalsky <michael.byalsky@gmail.com>
2020-11-26 03:36:16 +0200

    working compose

--- init-mongo.js
+++
@@ -1,5 +0,0 @@
-db.createUser({
-  user: "scrawler",
-  pwd: "scrawlerpassword",
-  roles: [{ role: "readWrite", db: "pastedb" }],
-});
--- src/main.py
+++ src/main.py
@@ -3,16 +3,11 @@ from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 from models import Paste
 from services import connect_to_mongodb
-import os
 import dotenv
 import time
 
 dotenv.load_dotenv()
 session = HTMLSession()
-
-# user = os.getenv('MONGO_INITDB_ROOT_USERNAME')
-# password = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
-# mongo_url = f'mongodb+srv://{user}:{password}@cluster0.d9oxu.mongodb.net/scrawler?retryWrites=true&w=majority'
 
 ARCHIVE_URL = 'https://pastebin.com/archive'
 MAIN_URL = 'https://pastebin.com'
@@ -49,8 +44,7 @@ def get_page_info(html):
     content_code = html.find('div', class_='source')
     for li in content_code.findAll('li'):
         for span in li.findAll('span'):
-            content += span.text
-        content += '\n'
+            content += span.text.strip()
     date_local = html.find('div', class_='date').span.text
     replace_date = date_local.replace('th,', '')
     date = str(arrow.get(replace_date, 'MMM D YYYY').to('UTC'))
@@ -62,15 +56,15 @@ def get_page_info(html):
 def insert_data_to_db(db_collection, data):
     try:
         db_collection.insert_one(data)
+        return "new paste added"
     except Exception as e:
-        print(e)
-    return 'data succefully updated, check your db for new data'
+        return e
 
 
 def remove_data_from_db(db_collection):
     try:
         db_collection.delete_many({})
-        return 'data succefully updated, check your db for new data' 
+        return "data deleted- ready to replace"
     except Exception as e:
         return e
 
@@ -78,14 +72,16 @@ def remove_data_from_db(db_collection):
 def main():
     print('scrawl started')
     pastes_collection = connect_to_mongodb()
-    remove_data_from_db(pastes_collection)
+    remove_msg = remove_data_from_db(pastes_collection)
+    print(remove_msg)
     parsed_html = fetch_page_html(ARCHIVE_URL)
     links = fetch_links_from_page(parsed_html)
     for link in links:
         link_html = fetch_page_html(f'{MAIN_URL}{link}')
         obj = get_page_info(link_html)
-        insert_data_to_db(pastes_collection, obj)
-    print('scrawl finished')
+        insert_msg = insert_data_to_db(pastes_collection, obj)
+        print(insert_msg)    
+    print(f'scrawl finished- added {str(len(links))} new pastes')
     time.sleep(120)
 
 
